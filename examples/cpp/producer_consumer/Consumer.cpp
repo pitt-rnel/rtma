a409a2bca51338e70fbcd70f4bd39bb7c132157c
Jeff Weiss <jmw182@pitt.edu>
2019-02-22 16:42:14 -0500

    added working cpp examples (adapted from dragonfly)

---
+++ examples/cpp/producer_consumer/Consumer.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "RTMA.h"
+#include "message_defs.h"
+
+
+int main( int argc, char *argv[])
+{
+	try 
+	{
+		RTMA_Module mod( MID_CONSUMER, 0);
+		mod.ConnectToMMM();
+		mod.Subscribe( MT_TEST_DATA);
+		
+        std::cout << "Consumer running...\n" << std::endl;
+        
+		while( 1) 
+		{
+            CMessage M;
+			mod.ReadMessage( &M);
+			std::cout << "Received message " << M.msg_type << std::endl;
+            
+			switch( M.msg_type) {
+				case MT_TEST_DATA:
+					MDF_TEST_DATA data;
+					M.GetData( &data);
+					std::cout << "  Data = [a: " << data.a << ", b: " << data.b << ", x: " << data.x << "]" << std::endl;
+					break;
+			}
+		}
+	}
+	catch( UPipeClosedException &e)
+	{
+		MyCString s;
+		e.AppendTraceToString( s);
+		std::cout << "UPipeClosedException: " << s.GetContent() << std::endl;
+	}
+	catch( UPipeException &e)
+	{
+		MyCString s;
+		e.AppendTraceToString( s);
+		std::cout << "UPipeException: " << s.GetContent() << std::endl;
+	}
+	catch( MyCException &e)
+	{
+		MyCString s;
+		e.AppendTraceToString( s);
+		std::cout << "MyCException: " << s.GetContent() << std::endl;
+	}
+	catch(...)
+	{
+		MyCString s;
+		std::cout << "Unknown Exception!" << std::endl;
+	}
+	
+	std::cout << "Exiting cleanly." << std::endl;
+}
---
+++ examples/cpp/producer_consumer/Producer.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "RTMA.h"
+#include "message_defs.h"
+
+int main( int argc, char *argv[])
+{
+	try 
+	{
+		RTMA_Module mod( MID_PRODUCER, 0);
+		mod.ConnectToMMM();
+        std::cout << "Producer running...\n" << std::endl;
+        
+        int a = 0;
+		while( 1) 
+        {
+            MDF_TEST_DATA data;
+            data.a = a++;
+            data.b = -3;
+            data.x = 1.234;
+
+            CMessage M( MT_TEST_DATA);
+            M.SetData( &data, sizeof(data));
+            mod.SendMessageRTMA( &M);
+
+			std::cout << "Sent message " << M.msg_type << 
+						 "   Data = [a: " << data.a << ", b: " << data.b << ", x: " << data.x << "]" << std::endl;
+            
+            Sleep( 1000);
+        }
+	}
+	catch( UPipeClosedException &e)
+	{
+		MyCString s;
+		e.AppendTraceToString( s);
+		std::cout << "UPipeClosedException: " << s.GetContent() << std::endl;
+	}
+	catch( UPipeException &e)
+	{
+		MyCString s;
+		e.AppendTraceToString( s);
+		std::cout << "UPipeException: " << s.GetContent() << std::endl;
+	}
+	catch( MyCException &e)
+	{
+		MyCString s;
+		e.AppendTraceToString( s);
+		std::cout << "MyCException: " << s.GetContent() << std::endl;
+	}
+	catch(...)
+	{
+		MyCString s;
+		std::cout << "Unknown Exception!" << std::endl;
+	}
+	
+	std::cout << "Exiting cleanly." << std::endl;
+}
