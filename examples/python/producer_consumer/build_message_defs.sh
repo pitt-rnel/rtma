b771d15000b4081825fd683c647f6b72011479ca
Jeff Weiss <weiss.jeffrey.m@gmail.com>
2019-02-28 17:10:46 -0500

    updated to build message defs without DRAGONFLY env var

--- examples/python/producer_consumer/build_message_defs.sh
+++ examples/python/producer_consumer/build_message_defs.sh
@@ -1,1 +1,7 @@
-python $DRAGONFLY/tools/build_python_message_defs.py message_defs.h
+#!/bin/bash
+#python $DRAGONFLY/tools/build_python_message_defs.py message_defs.h
+H2XML=~/svn/ctypeslib/scripts/h2xml.py
+XML2PY=~/svn/ctypeslib/scripts/xml2py.py
+${H2XML} -c -o `pwd`/message_defs.xml `pwd`/message_defs.h
+${XML2PY} -o `pwd`/message_defs.py `pwd`/message_defs.xml
+rm -f `pwd`/message_defs.xml
