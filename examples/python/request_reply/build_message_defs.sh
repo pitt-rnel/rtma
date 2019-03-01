e08d50631aa4b97180a272e94bac92a3056bc6dc
Jeff Weiss <weiss.jeffrey.m@gmail.com>
2019-03-01 16:38:26 -0500

    updated example build_message_defs script to work with python3/ctypeslib2

--- examples/python/request_reply/build_message_defs.sh
+++ examples/python/request_reply/build_message_defs.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 #python $DRAGONFLY/tools/build_python_message_defs.py message_defs.h
-H2XML=~/svn/ctypeslib/scripts/h2xml.py
-XML2PY=~/svn/ctypeslib/scripts/xml2py.py
-${H2XML} -c -o `pwd`/message_defs.xml `pwd`/message_defs.h
-${XML2PY} -o `pwd`/message_defs.py `pwd`/message_defs.xml
-rm -f `pwd`/message_defs.xml
+CLANG2PY=/usr/local/lib/python3.7/site-packages/ctypeslib/clang2py.py
+python3 ${CLANG2PY} -c -k cdefmstu -o `pwd`/message_defs.py `pwd`/message_defs.h
