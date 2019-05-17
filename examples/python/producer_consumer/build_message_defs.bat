6082f7018cbc09b0be8d5056cbe2ecf873ae775a
Jeff Weiss <jmw182@pitt.edu>
2019-05-17 10:22:54 -0400

    simplified example build_message_defs.bat

--- examples/python/producer_consumer/build_message_defs.bat
+++ examples/python/producer_consumer/build_message_defs.bat
@@ -1,3 +1,3 @@
 REM python %RTMA%/tools/build_python_message_defs.py message_defs.h
 set CLANG2PY="C:\Program Files\Python37\Lib\site-packages\ctypeslib\clang2py.py"
-python3 %CLANG2PY% -c -k cdefmstu -o %cd%\message_defs.py %cd%\message_defs.h
+python3 %CLANG2PY% -c -k cdefmstu -o message_defs.py message_defs.h
