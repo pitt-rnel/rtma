8bdae89719998eb3ade116f0e342d672d5af6860
Jeff Weiss <jmw182@pitt.edu>
2019-05-17 13:21:21 -0400

    Updated make_py_config to use CLANG2PY, output climber_config.py instead of RTMA_config2.py. Works with python2 or python3. Consider moving or copying to climber repo (include folder).

--- lang/python/make_py_config.bat
+++ lang/python/make_py_config.bat
@@ -1,1 +1,6 @@
-python C:\git\rtma\lang\python\ctypesgen/ctypesgen.py --includedir="C:\git\climber\include" -a -o RTMA_config2.py C:\git\climber\include\climber_config.h
+REM python C:\git\rtma\lang\python\ctypesgen/ctypesgen.py --includedir="C:\git\climber\include" -a -o RTMA_config2.py C:\git\climber\include\climber_config.h
+REM python3 C:\git\ctypesgen\run.py --includedir="C:\git\climber\include" -a -o RTMA_config2.py C:\git\climber\include\climber_config.h
+REM CLANG2PY works with either python2 or python3 and produces identical output, so only need to generate once
+set PYTHON=python
+set CLANG2PY="%PYTHON3_BASE%\Lib\site-packages\ctypeslib\clang2py.py"
+%PYTHON% %CLANG2PY% -c -k cdefmstu -i -o climber_config.py C:\git\climber\include\climber_config.h
