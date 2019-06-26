66759095fbeb260fbc5350e13aaa2036ce01be8f
Jeff Weiss <jmw182@pitt.edu>
2019-06-26 14:42:51 -0400

    Changed python config output directory to be in climber repo.

--- lang/python/make_py_config.bat
+++ lang/python/make_py_config.bat
@@ -3,4 +3,4 @@ REM python3 C:\git\ctypesgen\run.py --includedir="C:\git\climber\include" -a -o 
 REM CLANG2PY works with either python2 or python3 and produces identical output, so only need to generate once
 set PYTHON=python
 set CLANG2PY="%PYTHON3_BASE%\Lib\site-packages\ctypeslib\clang2py.py"
-%PYTHON% %CLANG2PY% -c -k cdefmstu -i -o climber_config.py D:\git\climber\include\climber_config.h
+%PYTHON% %CLANG2PY% -c -k cdefmstu -i -o D:\git\climber\include\climber_config.py D:\git\climber\include\climber_config.h
