446905cc972f57da9f6758fd400fb9df77ffd92b
Jeff Weiss <jmw182@pitt.edu>
2023-06-30 09:49:16 -0400

    Fixed error check: cannot send data message with SendSignal

--- lang/matlab/SendSignal.m
+++ lang/matlab/SendSignal.m
@@ -11,16 +11,16 @@ if( ~exist( 'DestHostID', 'var'))
     DestHostID = 0;
 end
 
+% Make sure that the specified message type is a signal (i.e. has no data)
+if( isfield( RTMA.MDF, MessageType))
+    error( ['MessageType ' MessageType ' is supposed to have data and cannot be sent using SendSignal']);
+end
+
 % If MessageType is a string, convert it to numeric Message Type ID
 % Also do the same for module and host ID
 MessageType = EnsureNumericMessageType( MessageType);
 DestModuleID = EnsureNumericModuleID( DestModuleID);
 DestHostID = EnsureNumericModuleID( DestHostID);
-
-% Make sure that the specified message type is a signal (i.e. has no data)
-if( isfield( RTMA.MDF, MessageType))
-    error( ['MessageType ' MessageType ' is supposed to have data and cannot be sent using SendSignal']);
-end
 
 [status, send_time, msg_count] = MatlabRTMA( RTMA.mex_opcode.SEND_SIGNAL, MessageType, DestModuleID, DestHostID);
 if( status == 0); error( 'Could not send signal'); end
