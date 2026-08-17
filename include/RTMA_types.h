afc8d1115eedeb21575eda1c3e428b602505fce2
Jeff Weiss <jmw182@pitt.edu>
2026-08-17 14:14:10 -0400

    resolve multi-line comment gcc warnings

--- include/RTMA_types.h
+++ include/RTMA_types.h
@@ -30,18 +30,18 @@ typedef int32_t UID;
 // Following macro was commented out for compatibility with ctypesgen2 v2.2.2
 // python package (used for Python3 compatiblity) All references to this macro
 // in this file and in RTMA.h were replaced with actual values
-// #define RTMA_MSG_HEADER_FIELDS \
-//	MSG_TYPE	msg_type; \
-//	MSG_COUNT	msg_count; \
-//	double	send_time; \
-//	double	recv_time; \
-//	HOST_ID		src_host_id; \
-//	MODULE_ID	src_mod_id; \
-//	HOST_ID		dest_host_id; \
-//	MODULE_ID	dest_mod_id; \
-//	int			num_data_bytes; \
-//	int			remaining_bytes; \
-//	int			is_dynamic; \
+// #define RTMA_MSG_HEADER_FIELDS
+//	MSG_TYPE	msg_type;
+//	MSG_COUNT	msg_count;
+//	double	send_time;
+//	double	recv_time;
+//	HOST_ID		src_host_id;
+//	MODULE_ID	src_mod_id;
+//	HOST_ID		dest_host_id;
+//	MODULE_ID	dest_mod_id;
+//	int			num_data_bytes;
+//	int			remaining_bytes;
+//	int			is_dynamic;
 //	int			reserved
 // msg_type - Message type ID
 // msg_count - Source message count (per source, starting from 1)
